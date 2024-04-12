453cabe6dafb5b89c671cee975068c7f3d96bb14
Chris Styles <christopherstyles@users.noreply.github.com>
2024-04-11 19:20:25 -0700

    Fix overflow issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="antialiased">
       <body className={inter.className}>
-        <main className="grid min-h-screen grid-rows-[auto,1fr,auto] dark:font-light">
+        <main className="grid min-h-screen grid-cols-1 grid-rows-[auto,1fr,auto] dark:font-light">
           <Navbar />
           <div className="mx-auto mt-20 flex w-full max-w-screen-2xl flex-col items-start px-4 md:px-6">
             {children}
