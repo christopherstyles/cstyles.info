714a3c1b01f4c01072d197fee4f23e18f7a957b8
Chris Styles <christopherstyles@users.noreply.github.com>
2024-04-11 19:43:52 -0700

    Lesson gap between stack tags

--- src/components/stack-list.tsx
+++ src/components/stack-list.tsx
@@ -2,7 +2,7 @@ import Tag from "./tag";
 
 export default function StackList({ stack }: { stack: string[] }): JSX.Element {
   return (
-    <ul className="flex w-full flex-wrap items-center gap-4 text-sm">
+    <ul className="flex w-full flex-wrap items-center gap-3 text-sm">
       {stack.map((item, index) => (
         <li
           className="rounded-full bg-neutral-200 px-4 py-2 text-neutral-600 dark:bg-neutral-800 dark:text-neutral-200"
