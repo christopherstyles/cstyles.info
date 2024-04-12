b8776f08066bb052ba74447d02ab90659984290b
Chris Styles <christopherstyles@users.noreply.github.com>
2024-04-11 20:12:59 -0700

    Adjust stack list tag spacing

--- src/components/stack-list.tsx
+++ src/components/stack-list.tsx
@@ -2,13 +2,12 @@ import Tag from "./tag";
 
 export default function StackList({ stack }: { stack: string[] }): JSX.Element {
   return (
-    <ul className="flex w-full flex-wrap items-center gap-3 text-sm">
+    <ul className="flex w-full flex-wrap items-center text-sm">
       {stack.map((item, index) => (
-        <li
-          className="rounded-full bg-neutral-200 px-4 py-2 text-neutral-600 dark:bg-neutral-800 dark:text-neutral-200"
-          key={`stack-${index}`}
-        >
-          {item}
+        <li className="mr-2 mt-3" key={`stack-${index}`}>
+          <div className="rounded-full bg-neutral-200 px-4 py-2 text-sm text-neutral-600 dark:bg-neutral-800 dark:text-neutral-200">
+            {item}
+          </div>
         </li>
       ))}
     </ul>
