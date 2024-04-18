87e464b4e49b32b40537207a2071678d218534b5
Chris Styles <christopherstyles@users.noreply.github.com>
2024-04-18 11:16:36 -0700

    Update lead typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,9 @@ export default async function Page({ params }: { params: { slug: string } }) {
 
   return (
     <>
-      <div className="mx-auto max-w-5xl">
+      <div className="max-w-5xl xl:mx-auto">
         <div className="mb-40 flex flex-col gap-12">
-          <p className="max-w-[50ch] text-2xl xl:text-3xl">
+          <p className="max-w-[48ch] text-pretty text-2xl xl:mx-auto xl:text-center">
             Chris Styles is a design-focused software engineer, based in Oregon.
             His work focuses on creating beautiful, functional, and accessible
             digital experiences.
