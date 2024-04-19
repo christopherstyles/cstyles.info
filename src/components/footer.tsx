03c50b4a6af354f1603abf5f8aa4c026236664eb
Chris Styles <christopherstyles@users.noreply.github.com>
2024-04-19 15:37:21 -0700

    Update footer text size

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Social from "./social";
 export default function Footer(): JSX.Element {
   return (
     <footer className="flex w-full items-center justify-between p-12 px-16">
-      <h1 className="text-xs text-neutral-600 dark:font-light">
+      <h1 className="text-lg text-neutral-600 dark:font-light">
         ©️ {new Date().getFullYear()}
       </h1>
       <nav className="flex gap-4">
