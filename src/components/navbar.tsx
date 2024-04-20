e64b1c2d5ad8afc10d34cc78573da18cd54744d1
Chris Styles <christopherstyles@users.noreply.github.com>
2024-04-20 01:13:01 -0700

    Wrap link with motion

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,6 +10,8 @@ import { VscArrowLeft } from "react-icons/vsc";
 import Menu from "@/components/menu";
 import Social from "./social";
 
+const MotionLink = motion(Link);
+
 export default function Navbar() {
   const [isMenuOpen, setMenuIsOpen] = useState(false);
   const pathname = usePathname();
@@ -31,29 +33,30 @@ export default function Navbar() {
   return (
     <header className="mx-auto flex w-full max-w-screen-2xl items-center justify-between p-12 px-4 md:px-6">
       <div className="relative flex h-full w-full">
-        <div className="absolute left-0 top-0">
-          <motion.div
+        <div className="absolute left-2 top-2">
+          <MotionLink
             animate={pathname === "/" ? "hidden" : "visible"}
+            aria-label="Go to the home page"
+            href="/"
             initial="initial"
+            title="Home page link"
             variants={variants}
             whileHover={{ scale: 1.4, x: 4 }}
           >
-            <Link
-              arial-label="Go to the home page"
-              href="/"
-              title="Home page link"
-            >
-              <div className="-ml-6 -mt-6 p-6">
-                <VscArrowLeft size={24} color="currentColor" />
-              </div>
-            </Link>
-          </motion.div>
+            <div className="-ml-6 -mt-6 p-2">
+              <VscArrowLeft size={24} color="currentColor" />
+            </div>
+          </MotionLink>
         </div>
       </div>
 
-      <nav className="hidden gap-4 text-base md:flex md:items-center dark:font-light">
-        <Link href="/about">about</Link>
-        <Link href="/cv">cv</Link>
+      <nav className="hidden gap-2 text-base md:flex md:items-center dark:font-light">
+        <Link className="px-2" href="/about">
+          about
+        </Link>
+        <Link className="px-2" href="/cv">
+          cv
+        </Link>
         <div>•</div>
         <Social />
       </nav>
