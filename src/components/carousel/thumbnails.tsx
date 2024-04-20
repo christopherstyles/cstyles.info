39dbb3a38f7aba6950fc1f89b3bd918d0e94bd95
Chris Styles <christopherstyles@users.noreply.github.com>
2024-04-20 12:56:24 -0700

    Extract thumbnail component

--- src/components/carousel/thumbnails.tsx
+++ src/components/carousel/thumbnails.tsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import { config } from "./config";
 import { Screenshot } from "../types";
 import { variants } from "./variants";
+import Thumbnail from "./thumbnail";
 
 interface ThumbnailsProps {
   images: Screenshot[];
@@ -34,28 +35,13 @@ export default function Thumbnails({
       }}
     >
       {images.map((image, i) => (
-        <motion.button
-          aria-label="Select this image as the current slide"
-          animate={i === index ? "active" : "inactive"}
-          className="shrink-0 overflow-hidden focus:outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-900 md:rounded-md dark:focus-visible:ring-neutral-200"
-          initial={false}
-          key={image.src}
+        <Thumbnail
+          active={i === index}
+          image={image}
+          key={i}
           onClick={() => setIndex(i)}
           onFocus={() => setIndex(i)}
-          variants={variants.thumbnail}
-          whileFocus="active"
-          whileHover={{ opacity: 1 }}
-        >
-          <Image
-            alt={`${image.alt}`}
-            className="h-full w-full object-cover"
-            draggable={false}
-            height={1728}
-            sizes="20vw"
-            src={image.src}
-            width={2880}
-          />
-        </motion.button>
+        />
       ))}
     </motion.div>
   );
