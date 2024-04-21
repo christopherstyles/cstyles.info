567b88f20741b837b6dba6aa714cde735eb18f97
Chris Styles <christopherstyles@users.noreply.github.com>
2024-04-21 14:20:15 -0700

    Move mobile toggle button to menu component

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,76 +1,100 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { RxEnvelopeClosed, RxLinkedinLogo } from "react-icons/rx";
 import { VscGithubInverted } from "react-icons/vsc";
+import { AnimatePresence, motion } from "framer-motion";
 
-interface MenuProps {
-  isMenuOpen: boolean;
-  setMenuIsOpen: (isOpen: boolean) => void;
-}
+export default function Menu() {
+  const [isMenuOpen, setMenuIsOpen] = useState(false);
 
-export default function Menu({ isMenuOpen, setMenuIsOpen }: MenuProps) {
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+  }, [isMenuOpen]);
+
+  console.log("isMenuOpen", isMenuOpen);
+
   return (
     <>
-      {isMenuOpen && (
-        <div
-          className="fixed inset-0 z-40 overflow-auto rounded-lg bg-neutral-100 px-12 py-6 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100"
-          key="menu"
-        >
-          <ul className="flex h-full flex-col justify-center gap-2 divide-y divide-neutral-900 text-center text-2xl ">
-            <li key="menu-about">
-              <Link
-                className="block p-6"
-                href="/about"
-                onClick={() => setMenuIsOpen(false)}
-              >
-                about
-              </Link>
-            </li>
-            <li key="menu-cv">
-              <Link
-                className="block p-6"
-                href="/cv"
-                onClick={() => setMenuIsOpen(false)}
-              >
-                cv
-              </Link>
-            </li>
-            <li className="p-6" key="menu-github">
-              <div className="flex items-center justify-center gap-2">
-                <a
-                  aria-label="Email Chris Styles (opens in the default mail application)"
-                  className="p-6"
-                  href="mailto:cstyles@hey.com"
-                  onClick={() => setMenuIsOpen(false)}
-                  rel="noreferrer noopener"
-                  target="_blank"
-                >
-                  <RxEnvelopeClosed size={24} />
-                </a>
-                <a
-                  aria-label="GitHub: Chris Styles (opens in a new tab)"
-                  className="p-6"
-                  href="https://www.github.com/christopherstyles"
-                  onClick={() => setMenuIsOpen(false)}
-                  rel="noreferrer noopener"
-                  target="_blank"
-                >
-                  <VscGithubInverted size={24} />
-                </a>
-                <a
-                  aria-label="LinkedIn: Chris Styles (opens in a new tab)"
-                  className="p-6"
-                  href="https://www.linkedin.com/in/christopherstyles"
-                  onClick={() => setMenuIsOpen(false)}
-                  rel="noreferrer noopener"
-                  target="_blank"
-                >
-                  <RxLinkedinLogo size={24} />
-                </a>
-              </div>
-            </li>
-          </ul>
+      <motion.button
+        aria-label="Toggle the mobile menu"
+        className="duration-400 group absolute right-4 top-10 z-50 flex h-11 w-11 origin-center touch-manipulation select-none items-center justify-center outline-none transition-all ease-out focus:outline-none focus-visible:ring focus-visible:ring-neutral-900 active:scale-110 md:hidden"
+        data-open={isMenuOpen}
+        onClick={() => setMenuIsOpen(!isMenuOpen)}
+        type="button"
+      >
+        <div className="relative flex h-8 w-8 flex-col items-center justify-center">
+          <div className="duration-400 absolute top-[10px] h-0.5 w-6 origin-center bg-neutral-900 transition-all ease-in-out group-data-[open=true]:top-[15px] group-data-[open=true]:rotate-45 dark:bg-neutral-100"></div>
+          <div className="duration-400 absolute top-[20px] h-0.5 w-6 origin-center bg-neutral-900 transition-all ease-in-out group-data-[open=true]:top-[15px] group-data-[open=true]:-rotate-45 dark:bg-neutral-100"></div>
         </div>
-      )}
+      </motion.button>
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.div
+            className="absolute inset-x-0 top-0 z-40 h-full w-full overflow-auto rounded-lg bg-neutral-100 px-12 py-6 text-neutral-900 md:hidden dark:bg-neutral-900 dark:text-neutral-100"
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "-100%", pointerEvents: "none" }}
+          >
+            <ul className="flex h-full flex-col justify-center gap-2 divide-y divide-neutral-900 text-center text-2xl ">
+              <li key="menu-about">
+                <Link
+                  className="block p-6"
+                  href="/about"
+                  onClick={() => setMenuIsOpen(false)}
+                >
+                  about
+                </Link>
+              </li>
+              <li key="menu-cv">
+                <Link
+                  className="block p-6"
+                  href="/cv"
+                  onClick={() => setMenuIsOpen(false)}
+                >
+                  cv
+                </Link>
+              </li>
+              <li className="p-6" key="menu-github">
+                <div className="flex items-center justify-center gap-2">
+                  <a
+                    aria-label="Email Chris Styles (opens in the default mail application)"
+                    className="p-6"
+                    href="mailto:cstyles@hey.com"
+                    onClick={() => setMenuIsOpen(false)}
+                    rel="noreferrer noopener"
+                    target="_blank"
+                  >
+                    <RxEnvelopeClosed size={24} />
+                  </a>
+                  <a
+                    aria-label="GitHub: Chris Styles (opens in a new tab)"
+                    className="p-6"
+                    href="https://www.github.com/christopherstyles"
+                    onClick={() => setMenuIsOpen(false)}
+                    rel="noreferrer noopener"
+                    target="_blank"
+                  >
+                    <VscGithubInverted size={24} />
+                  </a>
+                  <a
+                    aria-label="LinkedIn: Chris Styles (opens in a new tab)"
+                    className="p-6"
+                    href="https://www.linkedin.com/in/christopherstyles"
+                    onClick={() => setMenuIsOpen(false)}
+                    rel="noreferrer noopener"
+                    target="_blank"
+                  >
+                    <RxLinkedinLogo size={24} />
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import Link from "next/link";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { VscArrowLeft } from "react-icons/vsc";
 
@@ -12,16 +11,7 @@ import Social from "./social";
 const MotionLink = motion(Link);
 
 export default function Navbar() {
-  const [isMenuOpen, setMenuIsOpen] = useState(false);
   const pathname = usePathname();
-
-  useEffect(() => {
-    if (isMenuOpen) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "unset";
-    }
-  }, [isMenuOpen]);
 
   const variants = {
     initial: { opacity: 0 },
@@ -33,19 +23,24 @@ export default function Navbar() {
     <header className="mx-auto flex w-full max-w-screen-2xl items-center justify-between p-12 px-4 md:px-6">
       <div className="relative flex h-full w-full">
         <div className="absolute left-2 top-2">
-          <MotionLink
-            animate={pathname === "/" ? "hidden" : "visible"}
-            aria-label="Go to the home page"
-            href="/"
-            initial="initial"
-            title="Home page link"
-            variants={variants}
-            whileHover={{ scale: 1.4, x: 4 }}
-          >
-            <div className="-ml-6 -mt-6 p-2">
-              <VscArrowLeft size={24} color="currentColor" />
-            </div>
-          </MotionLink>
+          <AnimatePresence>
+            {pathname !== "/" && (
+              <MotionLink
+                animate={pathname === "/" ? "hidden" : "visible"}
+                aria-label="Go to the home page"
+                href="/"
+                initial="initial"
+                title="Home page link"
+                variants={variants}
+                whileHover={{ scale: 1.4, x: 4 }}
+                whileTap={{ scale: 1.4, x: 4 }}
+              >
+                <div className="-ml-6 -mt-6 p-2">
+                  <VscArrowLeft size={32} color="currentColor" />
+                </div>
+              </MotionLink>
+            )}
+          </AnimatePresence>
         </div>
       </div>
 
@@ -60,20 +55,7 @@ export default function Navbar() {
         <Social />
       </nav>
 
-      <button
-        aria-label="Toggle the mobile menu"
-        className="duration-400 group absolute right-4 top-10 z-50 flex h-11 w-11 origin-center touch-manipulation items-center justify-center outline-none transition-all ease-[cubic-bezier(0.08,0.6,0.54,0.92)] focus:outline-none focus-visible:ring focus-visible:ring-neutral-900 md:hidden"
-        data-open={isMenuOpen}
-        onClick={() => setMenuIsOpen(!isMenuOpen)}
-        type="button"
-      >
-        <div className="relative flex h-8 w-8 flex-col items-center justify-center">
-          <div className="duration-400 absolute top-[11px] h-0.5 w-6 origin-center bg-neutral-900 transition-all ease-[cubic-bezier(0.08,0.6,0.54,0.92)] group-data-[open=true]:top-[15px] group-data-[open=true]:rotate-45 dark:bg-neutral-100"></div>
-          <div className="duration-400 absolute top-[18px] h-0.5 w-6 origin-center bg-neutral-900 transition-all ease-[cubic-bezier(0.08,0.6,0.54,0.92)] group-data-[open=true]:top-[15px] group-data-[open=true]:-rotate-45 dark:bg-neutral-100"></div>
-        </div>
-      </button>
-
-      <Menu isMenuOpen={isMenuOpen} setMenuIsOpen={setMenuIsOpen} />
+      <Menu />
     </header>
   );
 }
