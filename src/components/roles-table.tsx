453b7d868df6a0c7d19aba1f727dec976ce7986a
Chris Styles <christopherstyles@users.noreply.github.com>
2024-04-29 01:48:14 -0700

    Use neutral colors

--- src/components/roles-table.tsx
+++ src/components/roles-table.tsx
@@ -7,7 +7,7 @@ interface RolesTableProps {
 
 export default function RolesTable({ roles }: RolesTableProps) {
   return (
-    <table className="max-w-fit divide-y divide-gray-700 break-words">
+    <table className="max-w-fit divide-y divide-neutral-700 break-words">
       <thead>
         <tr>
           <th
@@ -36,7 +36,7 @@ export default function RolesTable({ roles }: RolesTableProps) {
           </th>
         </tr>
       </thead>
-      <tbody className="divide-y divide-gray-800">
+      <tbody className="divide-y divide-neutral-800">
         {roles.map((role, index) => (
           <tr key={`role-${index}`}>
             <td className="content-start whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-0">
