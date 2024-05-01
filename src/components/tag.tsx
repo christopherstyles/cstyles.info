74446727cf487db159fd8f762cb1e8f0f4338a1b
Chris Styles <christopherstyles@users.noreply.github.com>
2024-04-30 23:44:38 -0700

    Standardize

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -5,8 +5,9 @@ interface TagProps {
 export default function Tag({ name }: TagProps) {
   return (
     <div className="flex items-center gap-2.5 whitespace-nowrap border border-white py-2 pl-4 pr-5 text-base">
-      <div className="inline-flex h-2 w-2 rounded-full border border-white"></div>
-      {name}
+      <div className="inline-flex h-2 w-2 rounded-full border border-white">
+        {name}
+      </div>
     </div>
   );
 }
