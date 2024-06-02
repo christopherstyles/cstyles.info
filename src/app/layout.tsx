f7ede91db8bde123d177d20627b9eedddf392cc0
Chris Styles <christopherstyles@users.noreply.github.com>
2024-06-02 11:52:59 -0700

    Sort imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
