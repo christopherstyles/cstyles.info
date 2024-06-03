b731fad4c71e59d9329e6943d87cf50266331176
Chris Styles <christopherstyles@users.noreply.github.com>
2024-06-03 11:40:43 -0700

    Enable hover states only on supported devices

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "class",
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     extend: {
       backgroundImage: {
