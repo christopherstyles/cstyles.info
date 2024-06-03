3cc56f9107629db6885c298c13cce0bcde3e6284
Chris Styles <christopherstyles@users.noreply.github.com>
2024-06-03 15:10:41 -0700

    Extract open-source content

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -319,39 +319,6 @@ export default function Page({ params }: PageProps) {
                 </li>
               </ul>
             </section>
-            <section className="flex flex-col gap-4">
-              <h2 className="text-2xl">Open Source</h2>
-              <ul className="list-disc pl-5">
-                <li>
-                  <a
-                    className="group"
-                    href="https://github.com/christopherstyles/ngp_van"
-                    target="noreferrer noopener"
-                    aria-label="GitHub: ngp_van Ruby wrapper for the NGP-VAN API (opens in a new tab)"
-                  >
-                    ngp_van: A Ruby wrapper for the NGP-VAN API{" "}
-                    <VscArrowRight
-                      className="inline align-text-top transition-transform group-hover:translate-x-1"
-                      size={22}
-                    />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="group"
-                    href="https://github.com/christopherstyles/vscode-html-entities"
-                    target="noreferrer noopener"
-                    aria-label="GitHub: ngp_van Ruby wrapper for the HTML Entities VSCode extension (opens in a new tab)"
-                  >
-                    HTML Entities (VSCode extension){" "}
-                    <VscArrowRight
-                      className="inline align-text-top transition-transform group-hover:translate-x-1"
-                      size={22}
-                    />
-                  </a>
-                </li>
-              </ul>
-            </section>
           </div>
         </div>
       </Container>
