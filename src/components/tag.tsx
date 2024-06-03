af4c573afba2f7884f507c6a03fef998561a5c9f
Chris Styles <christopherstyles@users.noreply.github.com>
2024-06-03 12:41:15 -0700

    Add a sm variant to tag component

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -1,10 +1,13 @@
 interface TagProps {
   name: string;
+  size?: "sm" | "md";
 }
 
-export default function Tag({ name }: TagProps) {
+export default function Tag({ name, size = "md" }: TagProps) {
   return (
-    <div className="flex items-center gap-2.5 whitespace-nowrap rounded-full border border-neutral-200 bg-neutral-100/50 px-4 py-2 pl-4 pr-5 text-sm text-neutral-700 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-300">
+    <div
+      className={`${size === "sm" ? "px-2.5 py-1 text-sm" : "px-4 py-2 text-sm"} flex items-center gap-2.5 whitespace-nowrap rounded-full border border-neutral-200 bg-neutral-100/50 text-neutral-700 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-300`}
+    >
       {name}
     </div>
   );
