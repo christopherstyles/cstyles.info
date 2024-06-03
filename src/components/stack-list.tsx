488ce79deceb30bebda07b0534bc15e93820b769
Chris Styles <christopherstyles@users.noreply.github.com>
2024-06-03 01:59:39 -0700

    Use tag component on stack lists

--- src/components/stack-list.tsx
+++ src/components/stack-list.tsx
@@ -1,3 +1,5 @@
+import Tag from "./tag";
+
 interface StackListProps {
   stack: string[];
 }
@@ -5,11 +7,9 @@ interface StackListProps {
 export default function StackList({ stack }: StackListProps) {
   return (
     <ul className="flex w-full flex-wrap items-center text-sm">
-      {stack.map((item, index) => (
-        <li className="mr-2 mt-3" key={`stack-${index}`}>
-          <div className="rounded-full bg-neutral-200 px-4 py-2 text-sm text-neutral-600 dark:bg-neutral-800 dark:text-neutral-200">
-            {item}
-          </div>
+      {stack.map((item) => (
+        <li className="mr-2 mt-3" key={`stack-${item}`}>
+          <Tag name={item} />
         </li>
       ))}
     </ul>
--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -4,10 +4,8 @@ interface TagProps {
 
 export default function Tag({ name }: TagProps) {
   return (
-    <div className="flex items-center gap-2.5 whitespace-nowrap border border-white py-2 pl-4 pr-5 text-base">
-      <div className="inline-flex h-2 w-2 rounded-full border border-white">
-        {name}
-      </div>
+    <div className="flex items-center gap-2.5 whitespace-nowrap rounded-full border border-neutral-200 bg-neutral-100/50 px-4 py-2 pl-4 pr-5 text-sm text-neutral-700 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-300">
+      {name}
     </div>
   );
 }
