e4cde25e3d7ccf7eb0b328a14ee348eb4c1e9dec
Chris Styles <christopherstyles@users.noreply.github.com>
2024-06-12 13:46:27 -0700

    Adjust theme toggle label positioning

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -15,7 +15,6 @@ export default function ThemeToggle() {
     setMounted(true);
   }, []);
 
-  // TODO: Show skeleton button while loading
   if (!mounted) {
     return null;
   }
@@ -40,7 +39,7 @@ export default function ThemeToggle() {
           </>
         )}
       </button>
-      <div className="pointer-events-none absolute -bottom-9 left-1/2 scale-95 whitespace-nowrap rounded px-1.5 py-0.5 text-xs capitalize text-neutral-900 opacity-0 transition-all duration-200 ease-[cubic-bezier(0.76,0,0.24,1)] [transform:rotateX(90deg)_translateY(0)_translateX(-50%)_scale(0.9)] group-hover/theme-toggle:opacity-100 group-hover/theme-toggle:[transform:rotateX(0deg)_translateY(-40%)_translateX(-50%)_scale(1)] dark:text-white">
+      <div className="pointer-events-none absolute right-4 top-1/2 scale-95 whitespace-nowrap rounded px-1.5 text-xs capitalize text-neutral-900 opacity-0 transition-all duration-200 ease-[cubic-bezier(0.76,0,0.24,1)] [transform:rotateX(90deg)_translateY(0)_translateX(-50%)_scale(0.9)] group-hover/theme-toggle:opacity-100 group-hover/theme-toggle:[transform:rotateX(0deg)_translateY(-40%)_translateX(-50%)_scale(1)] dark:text-white">
         {theme} Mode
       </div>
     </div>
