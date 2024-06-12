fb8d9bd6883c8b529ed46036202e9d78836f5677
Chris Styles <christopherstyles@users.noreply.github.com>
2024-06-12 13:46:53 -0700

    Adjust dark background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <html lang="en" className="antialiased" suppressHydrationWarning>
       <body
-        className={`${inter.className} bg-neutral-50 text-neutral-900 dark:bg-neutral-950 dark:font-light dark:text-neutral-50`}
+        className={`${inter.className} bg-neutral-50 text-neutral-900 dark:bg-neutral-900 dark:font-light dark:text-neutral-50`}
       >
         <ThemeProvider attribute="class" enableSystem={true}>
           <main className="grid min-h-screen grid-cols-1 grid-rows-[auto,1fr,auto]">
