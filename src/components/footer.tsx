36b1dfea7048734cd7ad98519878a3c6c6c02ac7
Chris Styles <christopherstyles@users.noreply.github.com>
2024-06-12 15:34:04 -0700

    Update footer labels

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,10 +3,11 @@ import Social from "./social";
 export default function Footer() {
   return (
     <footer className="mx-auto flex w-full max-w-screen-2xl items-center justify-between p-12 px-4 md:px-6">
-      <h1 className="text-lg text-neutral-600 dark:font-light">
+      <h2 className="sr-only">Footer</h2>
+      <p className="text-neutral-600 dark:font-light">
         ©️ {new Date().getFullYear()}
-      </h1>
-      <nav className="flex gap-4">
+      </p>
+      <nav aria-label="Find me on the web" className="flex gap-4">
         <Social size="lg" />
       </nav>
     </footer>
