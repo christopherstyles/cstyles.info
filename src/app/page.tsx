d22520e6c82b331cfa4aab2f3135adf2359b94fd
Chris Styles <christopherstyles@users.noreply.github.com>
2024-06-14 16:37:19 -0700

    Update screenshots layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,46 +27,46 @@ export default async function Page({ params }: PageProps) {
             functional, and accessible digital experiences.
           </p>
         </div>
-
-        <section className="mb-20 grid grid-cols-1 gap-x-15 gap-y-16 md:grid-cols-2">
-          {data
-            .sort((a: Project, b: Project) => a.position - b.position)
-            .map((project: Project, index: number) => (
-              <FadeUp key={project.slug}>
-                <Link
-                  className="flex h-full flex-col focus-visible:outline-offset-8"
-                  href={`/work/${project.slug}`}
-                >
-                  <figure
-                    className="group flex h-full flex-col gap-4 transition-transform duration-1000 ease-out hover:-translate-y-0.5"
-                    role="group"
-                  >
-                    <figcaption className="flex flex-col items-start gap-3">
-                      <h2 className="text-2xl font-medium leading-[18px]">
-                        {project.title}
-                      </h2>
-                      <p className="text-base font-normal opacity-40">
-                        {project.agency.prefix} {project.agency.name}
-                      </p>
-                    </figcaption>
-                    <div className="relative flex h-full w-full flex-col self-stretch bg-gradient-to-b">
-                      <Image
-                        alt={project.title}
-                        className="object-cover shadow-xl shadow-black/10 transition-all duration-1000 ease-out group-hover:-translate-y-1 group-hover:shadow-2xl group-hover:shadow-black/50"
-                        height={1728}
-                        loading={index < 4 ? "eager" : "lazy"}
-                        priority={index < 4}
-                        sizes="(max-width: 768px) 100vw, (min-width: 769px) 50vw"
-                        src={`${project.posterImages[0].src}`}
-                        width={2880}
-                      />
-                    </div>
-                  </figure>
-                </Link>
-              </FadeUp>
-            ))}
-        </section>
       </Container>
+
+      <section className="mb-20 grid grid-cols-1 gap-x-15 gap-y-16 md:grid-cols-2 xl:grid-cols-3">
+        {data
+          .sort((a: Project, b: Project) => a.position - b.position)
+          .map((project: Project, index: number) => (
+            <FadeUp key={project.slug}>
+              <Link
+                className="flex h-full flex-col focus-visible:outline-offset-8"
+                href={`/work/${project.slug}`}
+              >
+                <figure
+                  className="group flex h-full flex-col gap-4 transition-transform duration-1000 ease-out hover:-translate-y-0.5"
+                  role="group"
+                >
+                  <figcaption className="flex flex-col items-start gap-3">
+                    <h2 className="text-2xl font-medium leading-[18px]">
+                      {project.title}
+                    </h2>
+                    <p className="text-base font-normal opacity-40">
+                      {project.agency.prefix} {project.agency.name}
+                    </p>
+                  </figcaption>
+                  <div className="relative flex h-full w-full flex-col self-stretch bg-gradient-to-b from-neutral-200 to-neutral-300 p-12 dark:from-neutral-600 dark:to-neutral-700">
+                    <Image
+                      alt={project.title}
+                      className="scale-95 rounded object-cover shadow-xl shadow-black/10 transition-all duration-300 ease-out group-hover:-translate-y-1 group-hover:scale-[1.03] group-hover:shadow-2xl group-hover:shadow-black/30 dark:group-hover:shadow-black/80"
+                      height={1728}
+                      loading={index < 4 ? "eager" : "lazy"}
+                      priority={index < 4}
+                      sizes="(max-width: 768px) 100vw, (min-width: 769px) 50vw"
+                      src={`${project.posterImages[0].src}`}
+                      width={2880}
+                    />
+                  </div>
+                </figure>
+              </Link>
+            </FadeUp>
+          ))}
+      </section>
     </div>
   );
 }
