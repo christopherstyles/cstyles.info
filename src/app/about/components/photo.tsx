d26e4dbe024dfcfeb0bfb1fa6b7f2dbb14553dbc
Chris Styles <christopherstyles@users.noreply.github.com>
2024-06-19 00:39:48 -0700

    Update photos display

--- src/app/about/components/photo.tsx
+++ src/app/about/components/photo.tsx
@@ -1,26 +1,22 @@
+import clsx from "clsx";
 import Image, { type StaticImageData } from "next/image";
 
 interface PhotoProps {
+  className?: string;
   image: StaticImageData;
   title: string;
 }
 
-export default function Photo({ image, title }: PhotoProps) {
+export default function Photo({ className, image, title }: PhotoProps) {
   return (
-    <div className="grid w-full grid-cols-1 items-start justify-end gap-4 md:grid-cols-[1fr_60%] md:gap-8">
-      <div className="order-last font-mono text-sm uppercase text-neutral-400 dark:text-neutral-500 md:sticky md:top-10 md:order-first">
-        {title}
-      </div>
-
-      <div className="relative flex justify-end">
-        <Image
-          className="w-full"
-          loading="lazy"
-          src={image}
-          alt={title}
-          quality={100}
-        />
-      </div>
+    <div className={clsx("relative flex justify-end", className)}>
+      <Image
+        className="w-full"
+        loading="lazy"
+        src={image}
+        alt={title}
+        quality={100}
+      />
     </div>
   );
 }
--- src/app/about/components/photos.tsx
+++ src/app/about/components/photos.tsx
@@ -5,14 +5,14 @@ import enchantments from "../../../../public/images/bio/enchantments3.jpg";
 import finley from "../../../../public/images/bio/finley4.jpg";
 import foxglove from "../../../../public/images/bio/foxglove.jpg";
 import gunnar from "../../../../public/images/bio/gunnar4.jpg";
+import hoskins from "../../../../public/images/bio/hoskins.jpg";
 import lucy from "../../../../public/images/bio/lucy2.jpg";
 import trillium from "../../../../public/images/bio/trillium.jpg";
 import Photo from "./photo";
 
 export default function Photos() {
   return (
-    <div className="flex flex-col gap-8">
-      <Photo image={enchantments} title="PNW Wonderland" />
+    <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3">
       <Photo image={finley} title="Finley" />
       <Photo image={lucy} title="Lucy" />
       <Photo image={gunnar} title="Gunnar" />
@@ -20,6 +20,12 @@ export default function Photos() {
       <Photo image={ducks} title="Duck Fam" />
       <Photo image={trillium} title="Trillium at Beazell Memorial Forest, OR" />
       <Photo image={foxglove} title="Foxglove at Fort Hoskins, OR" />
+      <Photo
+        className="md:hidden"
+        image={enchantments}
+        title="PNW Wonderland"
+      />
+      <Photo image={hoskins} title="Fort Hoskins, OR" />
       <Photo image={beach} title="Beach Combing" />
     </div>
   );
